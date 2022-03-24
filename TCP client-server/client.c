ecdd1277e01b4e9e7bfb64be894ccd35efe79137
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2022-03-24 09:31:58 +0300

    Update client.c

--- TCP client-server/client.c
+++ TCP client-server/client.c
@@ -3,32 +3,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <unistd.h>
-#include "erproc.h"
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include "erproc.h"
 
-int main() {
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+int main(){
+    int server = socket(AF_INET, SOCK_STREAM,0);
     struct sockaddr_in adr = {0};
     adr.sin_family = AF_INET;
-    adr.sin_port = htons(34543);
-    Inet_pton(AF_INET, "127.0.0.1", &adr.sin_addr);
-    Connect(fd, (struct sockaddr *) &adr, sizeof adr);
-    write(fd, "Hello, my dear friend!\n", 23);
-    char buf[256];
+    adr.sin_port =  htons(34543);
+    Bind(server, (struct sockaddr *) &adr, sizeof adr);
+    Listen(server, 5);
+    printf("server: waiting for connections...\n");
+    socklen_t adrlen = sizeof adr;
+    int fd = Accept(server, (struct sockaddr *) &adr,&adrlen);
     ssize_t nread;
+    char buf[256];
     nread = read(fd, buf, 256);
-    if (nread==-1){
+    if (nread == -1){
         perror("read failed");
         exit(EXIT_FAILURE);
     }
-    if (nread==0){
-        printf("EOF occured\n");
+    if (nread ==0){
+        printf("END OF FILE occured\n");
     }
-    write(STDOUT_FILENO, buf, nread);
-    //sleep(10);
+    printf("server: received '%s'\n",buf);
+    //write(STDOUT_FILENO, "thanks", 7);
+    write(fd, "thanks", 7);
+    //sleep(15);
     //close(fd);
+    //close(server);
     return 0;
 }
 
