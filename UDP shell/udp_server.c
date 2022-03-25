24be26c87ec781afc3ae14b3b4a609defb925d3e
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2022-03-25 22:18:10 +0300

    Update udp_server.c

--- UDP shell/udp_server.c
+++ UDP shell/udp_server.c
@@ -10,10 +10,9 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 int main(){
     int fds;
-    int server = socket(AF_INET, SOCK_DGRAM,0);
+    int server = Socket(AF_INET, SOCK_DGRAM,0);
     struct sockaddr_in adr = {0};
     adr.sin_family = AF_INET;
     adr.sin_port =  htons(34543);
@@ -21,34 +20,34 @@ int main(){
     Bind(server, (struct sockaddr *) &adr, sizeof adr);
     printf("server: waiting for connections...\n");
     socklen_t adrlen = sizeof adr;
-    char str[255];
-    FILE *fd3;
+    char command[255];
     FILE *fd2;
+    FILE *fd3;
     FILE *fd4;
     int fd1;
-    char cc[512];
+    char str[255];
     fd1= open("out.txt", O_CREAT|O_WRONLY);
-    int fd = recvfrom(server, str,255,0, (struct sockaddr *) &adr,&adrlen);
+    int client = recvfrom(server, command,255,0, (struct sockaddr *) &adr,&adrlen);
     dup2(fd1,1);
-    system(str);
-    int n=0;
+    dup2(fd1,2);
+    system(command);
+    int n=0; //number of lines in the file
     fd2= fopen("out.txt", "r");
-    while((fgets(cc, 512, fd2))!=NULL){
+    while((fgets(str, 255, fd2))!=NULL){
         n+=1;
     }
     char num[1024];
-    printf("numbers: %d", n);
     sprintf(num,"%d",n);
-    int numbytes1 = sendto(server, num, strlen(num), 0, (struct sockaddr *) &adr, sizeof adr);
+    int numberoflines = sendto(server, num, strlen(num), 0, (struct sockaddr *) &adr, sizeof adr);
     close(fd1);
     fclose(fd2);
     fd3 = fopen("out.txt", "r");
-    while((fgets(cc, 256, fd3))!=NULL){
-        int numbytes = sendto(server, cc, strlen(cc), 0, (struct sockaddr *) &adr, sizeof adr);
+    while((fgets(str, 256, fd3))!=NULL){
+        int sendstr = sendto(server, str, strlen(str), 0, (struct sockaddr *) &adr, sizeof adr);
+        memset(&str, 0, sizeof(str));
     }
-    fd4= fopen("out.txt", "w");
+    fd4= fopen("out.txt", "w"); //clean our output file
     fclose(fd4);
-    sleep(150);
     close(server);
     return 0;
 }
