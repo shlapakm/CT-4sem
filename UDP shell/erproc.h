0d2512814583619f74c6726112f13bf882f0d42a
Mariia Shlapak <81107214+shlapakm@users.noreply.github.com>
2022-03-26 14:45:02 +0300

    Update erproc.h

--- UDP shell/erproc.h
+++ UDP shell/erproc.h
@@ -1,11 +1,10 @@
 #include <sys/types.h>
-//#ifndef ERPROC_H
-//#define ERPROC_H
+#ifndef ERPROC_H
+#define ERPROC_H
 #include <sys/socket.h>
 int Socket(int domain, int type, int protocol);
 void Bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 void Listen(int sockfd, int backlog);
 int Accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
-void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+int Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 void Inet_pton(int af, const char *src, void *dst);
-
